e614119d30ddbd1892f31ad4ed6dd7ebf08aa6ca
Wei Zhou <wzhou8@gmail.com>
2020-12-21 14:39:50 -0500

    add kevin piano2

--- js/video.js
+++ js/video.js
@@ -1,6 +1,7 @@
 var videoSrcs = [
    "https://firebasestorage.googleapis.com/v0/b/kate-website-70fda.appspot.com/o/kevin-piano1.mp4?alt=media&token=1e7d84de-5743-471f-a8c2-a60eb7b805c3",
     "./videos/kevin_piano1_480_40.mp4",
+    "./videos/kevin_piano2-480.mp4",
     "./videos/kate-skate1.mp4"
 ]
 
